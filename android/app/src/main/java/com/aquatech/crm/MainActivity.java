82638c5533fb60e9cc720f4bb5ffd2ef339a5df0
sdsd11111 <cristhopheryeah113@gmail.com>
2026-05-18 21:03:49 -0500

    Explicitly register SyncBridgePlugin in MainActivity for secure bridge activation

--- android/app/src/main/java/com/aquatech/crm/MainActivity.java
+++ android/app/src/main/java/com/aquatech/crm/MainActivity.java
@@ -1,5 +1,13 @@
 package com.aquatech.crm;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.aquatech.crm.sync.SyncBridgePlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerPlugin(SyncBridgePlugin.class);
+    }
+}
